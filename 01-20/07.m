3f2d27750023f3874323151b8137862049efcea1
luozhexin <44456331+luozhexin@users.noreply.github.com>
2020-01-14 21:18:54 +0800

    Update 07.m

--- 01-20/07.m
+++ 01-20/07.m
@@ -1,16 +1,16 @@
-Remove the nth column from input matrix A and return the resulting matrix in output B.
+%Remove the nth column from input matrix A and return the resulting matrix in output B.
 
-So if
+%So if
 
- A = [1 2 3;
-      4 5 6];
-and
+ %A = [1 2 3;
+  %    4 5 6];
+%and
 
- n = 2 
-then B is
+ %n = 2 
+%then B is
 
- [ 1 3  
-   4 6 ]
+ %[ 1 3  
+  % 4 6 ]
    
    
    
