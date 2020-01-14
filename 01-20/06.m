477d26da95049b3952f0f843992d836fa77de2a8
luozhexin <44456331+luozhexin@users.noreply.github.com>
2020-01-14 21:17:09 +0800

    Create 06.m

---
+++ 01-20/06.m
@@ -0,0 +1,14 @@
+%Write a function which returns every other element of the vector passed in. That is, it returns the all odd-numbered elements, starting with the first.
+
+%Examples:
+
+ %Input  x = [1 3 2 4 3 5]
+ %Output y is [1 2 3]
+ %Input  x = [5 9 3 2 2 0 -1]
+ %Output y is [5 3 2 -1]
+ 
+ 
+ 
+ function y = everyOther(x)
+y=x(1:2:end);
+end
