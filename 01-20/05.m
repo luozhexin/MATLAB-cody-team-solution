1658c6ca47c202c86e9cf7467c542a75652397f9
luozhexin <44456331+luozhexin@users.noreply.github.com>
2020-01-14 21:15:23 +0800

    Create 05.m

---
+++ 01-20/05.m
@@ -0,0 +1,20 @@
+%Triangle numbers are the sums of successive integers. So 6 is a triangle number because
+
+ %6 = 1 + 2 + 3
+%which can be displayed in a triangular shape like so
+
+ %     *
+ %    * *
+ %   * * * 
+%Thus 6 = triangle(3). Given n, return t, the triangular number for n.
+
+%Example:
+
+ %Input  n = 4
+ %Output t is 10
+ 
+ 
+ 
+ function t = triangle(n)
+ t = n*(n+1)/2;
+end
