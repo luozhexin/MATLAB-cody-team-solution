5be7d79eda80cf562e46a46184455d53b83857be
luozhexin <44456331+luozhexin@users.noreply.github.com>
2020-01-14 21:18:17 +0800

    Create 07.m

---
+++ 01-20/07.m
@@ -0,0 +1,20 @@
+Remove the nth column from input matrix A and return the resulting matrix in output B.
+
+So if
+
+ A = [1 2 3;
+      4 5 6];
+and
+
+ n = 2 
+then B is
+
+ [ 1 3  
+   4 6 ]
+   
+   
+   
+   function B = column_removal(A,n)
+  A(:,n)=[]
+  B = A;
+end
